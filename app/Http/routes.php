6784d242b09adb268b2f00f152b00184995e19f1
senzdamsik <fakhri_kurniawan1@yahoo.com>
2020-05-02 23:08:30 +0700

    video 10

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+if(version_compare(PHP_VERSION, '7.2.0', '>=')) { error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING); }
 
 Route::get('/', [
     'uses' => 'BlogController@index',    
@@ -8,3 +9,4 @@ Route::get('/', [
 Route::get('/blog/show', function() {
     return view('blog.show');
 });
+
